72e4a39faeb66ee235b1282c14b336074315d9dc
William Henney <will@henney.org>
2022-04-29 21:45:59 -0500

    Copy the source files from muse-hii-regions/lib/whispy
    Only change so far is to get rid of relative import for
    tetrabloks. Now it imports directly from environment

--- src/whispy/__init__.py
+++ src/whispy/__init__.py
@@ -1,1 +1,11 @@
-"""Whispy."""
+"""
+Utilities for analyzing MUSE spectra (and from other spectrographs too)
+
+This package now has a name ...
+
+Whispy - Will Henney Imaging Spectroscopy in Python
+
+
+Will Henney, 2021, 2022
+
+"""
